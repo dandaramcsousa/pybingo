56c614202b17ccbe50261e0536a125991bc50f84
Dandara Sousa <dandaramcsousa@gmail.com>
2020-11-18 00:09:54 -0300

    add funções que a gente acredita que deve ter

---
+++ geradordecartela.py
@@ -0,0 +1,14 @@
+''' estruturar a cartela em uma matriz de 4x5
+    com valores de 1-90
+'''
+
+
+''' fazer um track de infos da pessoa dona da cartela e quantas faltam para bater
+    promovendo assim um "tem X pessoas armadas!"
+'''
+
+'''
+    main: dado as informações da pessoa dona da cartela,
+    gerar uma cartela e salvar num arquivo as informações dela
+    e as informações da cartela dela com um id único
+'''
